8e15bfefce4827e3ab2f76f447a98988500e2ff6
AjaxGb <ajaxgb@gmail.com>
2019-05-29 19:09:11 -0400

    Add support for custom Python parsers
    Add support for custom site parsers, Mangarock

---
+++ custom/awful_hospital.py
@@ -0,0 +1,54 @@
+import aiohttp
+from datetime import datetime
+import re
+import html
+import rfeed
+
+async def make_feed(request, gen_name):
+	base_url = 'http://www.bogleech.com/awfulhospital/archive.html'
+	async with aiohttp.request('GET', base_url) as resp:
+		if resp.status != 200:
+			raise NotFound(f'Awful Hospital seems to be down right now')
+		story_html = await resp.text()
+		last_modified = resp.headers.get('Last-Modified')
+		if last_modified:
+			last_modified = datetime.strptime(
+				last_modified,
+				'%a, %d %b %Y %H:%M:%S %Z')
+		else:
+			last_modified = datetime.now()
+	
+	# The HTML is so gunked up even BeautifulSoup won't cut it
+	# May the old gods forgive me
+	
+	layers = []
+	
+	for match in re.finditer(r'<a href="(.*?)">(.*?)<br>', story_html):
+		link, title = match.group(1, 2)
+		title = html.unescape(
+			re.sub(r'<.*?>',
+				lambda m: '' if m.group() == '</a>' else None,
+				title))
+		
+		layers.append(rfeed.Item(
+			title=title,
+			link=link))
+	
+	layers.reverse()
+	
+	return rfeed.Feed(
+			title='Awful Hospital',
+			description='Seriously the worst ever.',
+			link='http://www.bogleech.com/awfulhospital/',
+			lastBuildDate=last_modified,
+			pubDate=last_modified,
+			generator=gen_name,
+			items=layers)
+
+if __name__ == '__main__':
+	import asyncio
+	
+	loop = asyncio.get_event_loop()
+	feed = loop.run_until_complete(make_feed(None, 'AAA'))
+	print(feed.rss())
+	
